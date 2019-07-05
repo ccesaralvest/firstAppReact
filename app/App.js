9d5b54dee041ca740d6b0f636b52664bfb240e2d
ccesar <ccesar@MacBook-Pro-de-ccesar.local>
2019-07-05 06:02:17 -0300

    First component

--- app/App.js
+++ app/App.js
@@ -1,9 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactDOM = require('react-dom');
+var MeuComponente = require('./components/MyComponent.js');
 
 ReactDOM.render(
-    <div>
-        <h1>Hello World</h1>
-        <p>Testes</p>
-    </div>, document.getElementById('app')
+    React.createElement(MeuComponente),
+    document.getElementById('app')
 );
---
+++ app/components/MyComponent.js
@@ -0,0 +1,9 @@
+var React = require('react'); // importa a lib react
+
+class MyComponent extends React.Component {
+    render() {
+        return <h1>TESTE</h1>;
+    }
+}
+
+module.exports = MyComponent;
